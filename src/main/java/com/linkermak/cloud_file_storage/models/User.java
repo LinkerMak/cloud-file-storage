bf47ba21398d52a087e246c412f16480d35ed6fa
LinkerM <105450527+LinkerMak@users.noreply.github.com>
2026-07-03 19:57:13 +0400

    initial commit

---
+++ src/main/java/com/linkermak/cloud_file_storage/models/User.java
@@ -0,0 +1,19 @@
+package com.linkermak.cloud_file_storage.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password_hash", nullable = false, length = 100)
+    private String passwordHash;
+}

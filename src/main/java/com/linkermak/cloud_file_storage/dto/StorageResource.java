4033229d472050b9d4441d18ecd2a88993a24f57
LinkerMak <Linkermak@mail.ru>
2026-07-13 00:41:21 +0400

    commit 8. feat(core). Realized endpoint post /directory - create directory

---
+++ src/main/java/com/linkermak/cloud_file_storage/dto/StorageResource.java
@@ -0,0 +1,15 @@
+package com.linkermak.cloud_file_storage.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StorageResource {
+    private String path;
+    private String name;
+    private Integer size;
+    private StorageResourceType type;
+}
--- src/main/java/com/linkermak/cloud_file_storage/repositories/MinioObjectStorageRepository.java
+++ src/main/java/com/linkermak/cloud_file_storage/repositories/MinioObjectStorageRepository.java
@@ -2,12 +2,10 @@ package com.linkermak.cloud_file_storage.repositories;
 
 import com.linkermak.cloud_file_storage.config.minio.MinioProperties;
 import com.linkermak.cloud_file_storage.exceptions.StorageException;
-import com.linkermak.cloud_file_storage.models.StorageResource;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.errors.MinioException;
+import io.minio.*;
+import io.minio.errors.ErrorResponseException;
+import io.minio.messages.Item;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.io.ByteArrayInputStream;
@@ -36,6 +34,53 @@ public class MinioObjectStorageRepository implements ObjectStorageRepository{
             );
         } catch(Exception e) {
             throw new StorageException("Failed to create directory", e);
+        }
+    }
+
+    @Override
+    public boolean existsDirectory(Long id, String directoryPath) {
+        String key = pathToKey(id, directoryPath);
+
+        if(objectExists(key)) {
+            return true;
+        }
+
+        try{
+            Iterable<Result<Item>> results = minioClient.listObjects(
+                    ListObjectsArgs.builder()
+                            .bucket(bucket)
+                            .prefix(key)
+                            .maxKeys(1)
+                            .recursive(true)
+                            .build()
+            );
+
+            if(results.iterator().hasNext()) {
+                return true;
+            }
+
+            return false;
+        } catch(Exception e) {
+            throw new StorageException("Failed to check object existence:" + key, e);
+        }
+    }
+
+    private boolean objectExists(String key) {
+        try{
+            minioClient.statObject(
+                    StatObjectArgs.builder()
+                            .bucket(bucket)
+                            .object(key)
+                            .build()
+            );
+            return true;
+        } catch(ErrorResponseException e) {
+            if("NoSuchKey".equals(e.errorResponse().code())) {
+                return false;
+            }
+            throw new StorageException("Failed to check object existence:" + key, e);
+        } catch(Exception e) {
+            throw new StorageException("Failed to check object existence:" + key, e);
         }
     }
 
